d2221dfa2ed244b9a421880b36c655078825755b
Radminas Usackas <usackasr@gmail.com>
2021-11-15 13:12:21 +0000

    Added Routes, Controllers

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var mongoose = require('mongoose');
 // Routes
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var catalogRouter = require('./routes/catalog');  //Import routes for "catalog" area of site
 
 var app = express();
 
@@ -31,6 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Routers
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter);  // Add catalog routes to middleware chain.
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
